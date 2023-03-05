54ccf8d07ce9062441911464e1a5c441af2e129a
Solomon Enanaw Kebede <101396246+solomonenanaw@users.noreply.github.com>
2023-03-05 20:14:12 +0300

    Update app.py
    all

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/product/<int:product_id>')
+def product(product_id):
+    return render_template('product.html', product_id=product_id)
+
+if __name__ == '__main__':
+    app.run(debug=True)
